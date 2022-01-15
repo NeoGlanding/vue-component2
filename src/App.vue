0ea54679d86485433c06502917cbc0cb2dddc2d7
NeoGlanding <neoglanding@protonmail.com>
2022-01-15 13:23:43 +0700

    auto commit without message

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PropsEvent @get-lower-case="clgLowerCaseUpper" v-for="(el, index) in friends" :key="index" :username="el.username" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import PropsEvent from './components/PropsEvent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    PropsEvent
+  },
+  data() {
+    return {
+      friends: [
+        {
+          username: 'Wildan'
+        },
+        {
+          username: 'Daffa'
+        },
+        {
+          username: 'Raka'
+        }
+      ]
+    }
+  },
+  methods: {
+    clgLowerCaseUpper(lower, upper) {
+      console.log(lower, upper)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PropsEvent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div @click="getLowerCase" class="">
+        <p :class="{odd: username.length % 2 === 0}">{{ username }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        username: {
+            type: String
+        }
+    },
+    methods: {
+        getLowerCase() {
+            this.$emit('get-lower-case', this.username.toLowerCase(), this.username.toUpperCase())
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: aqua;
+    }
+
+    .odd {
+        color: red;
+    }
+</style>
